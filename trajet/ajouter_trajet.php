f7bfb151f568bb91f17bf9a1742805f379ec1b04
douaa <douaazouaoui0@gmail.com>
2024-02-02 22:37:06 +0100

    resrvation

--- trajet/ajouter_trajet.php
+++ trajet/ajouter_trajet.php
@@ -6,10 +6,10 @@ include '../index.php';
     $lieu_depart = $_POST["lieu_depart"];
     $lieu_arrive =  $_POST["lieu_arrive"];
     $date_depart =  $_POST["date_depart"];
-    $heur_depart =  $_POST["heur_depart"];
+    $heure_depart =  $_POST["heure_depart"];
     $nb_place =  $_POST["nb_place"];
     // Requête SQL pour insérer une nouvelle ligne dans la table (à ajuster selon votre cas)
-    $sql = "INSERT INTO trajet VALUES (,$chauffeur, $lieu_depart, $lieu_arrive, $date_depart,$heur_depart, $nb_place)";
+    $sql = "INSERT INTO trajet VALUES (,$chauffeur, $lieu_depart, $lieu_arrive, $date_depart,$heure_depart, $nb_place)";
 
     if ($conn->query($sql) === TRUE) {
         echo "Nouvelle ligne insérée avec succès.";
--- trajet/modifier_trajet.php
+++ trajet/modifier_trajet.php
@@ -7,14 +7,15 @@ $chauffeur =  $_POST["chauffeur"];
 $lieu_depart = $_POST["lieu_depart"];
 $lieu_arrive =  $_POST["lieu_arrive"];
 $date_depart =  $_POST["date_depart"];
-$heur_depart =  $_POST["heur_depart"];
+$heure_depart =  $_POST["heure_depart"];
+$hobdomadaire =  $_POST["hobdomadaire"];
 $nb_place =  $_POST["nb_place"];
 // Paramètres de connexion à la base de données
 
 include '../index.php';
 
 // Requête SQL pour mettre à jour l'entrée
-$requete = "UPDATE trajet SET Chauffeur=$chauffeur,Lieu_depar= $lieu_depart, Lieu_arrive=$lieu_arrive, Date_depart=$date_depart,Heur_depart=$heur_depart, Nbr_place_max=$nb_place where id_trajet=$ID";
+$requete = "UPDATE trajet SET Chauffeur=$chauffeur,Lieu_depar= $lieu_depart, Lieu_arrive=$lieu_arrive, Date_depart=$date_depart,Heure_depart=$heure_depart,hobdomadaire=$hobdomadaire, Nbr_place_max=$nb_place where id_trajet=$ID";
 $conn->query($requete);
 
 // Fermeture de la connexion
